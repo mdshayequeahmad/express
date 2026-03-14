ea0686eac8b678ad6a844b14162d041dfba6249f
mdshayequeahmad <shayeque01@gmail.com>
2026-03-14 17:10:17 +0530

    Unknown route

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
-app.get('/orders', (req, res) => {
-    res.send('Here is the list of all orders.');
+app.get('/products', (req, res) => {
+    res.send('Here is the list of all products.');
 });
 
-app.post('/orders', (req, res) => {
-    res.send('A new order has been created.');
+app.post('/products', (req, res) => {
+    res.send('A new product has been added.');
 });
 
-app.get('/users', (req, res) => {
-    res.send('Here is the list of all users.');
+app.get('/categories', (req, res) => {
+    res.send('Here is the list of all categories.');
 });
 
-app.post('/users', (req, res) => {
-    res.send('A new user has been added.');
+app.post('/categories', (req, res) => {
+    res.send('A new category has been created.');
+});
+
+app.use((req, res) => {
+    res.status(404).send('<h1>404 - Page Not Found</h1>');
 });
 
 app.listen(PORT, () => {

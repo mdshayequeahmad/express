d69b2197d85adc1b1e8011c78d5b98a5c1013ec0
mdshayequeahmad <shayeque01@gmail.com>
2026-03-15 23:47:48 +0530

    Dynamic Routes

--- app.js
+++ app.js
@@ -2,30 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-//Authentication middleware
-app.use((req, res, next) => {
-    console.log('Authentication middleware called!');
-    next();
+app.get('/', (req, res) => {
+    res.send('Hello from Home Page');
 });
 
-app.use('/library-2', (req, res, next) => {
-    console.log('Book recomendation');
-    next();
+app.get('/contact/:userId', (req, res) => {
+    const id = req.params.userId;
+    const name = req.query.name;
+    const age = req.query.age;
+    res.send(`Hello from contact Page for user ${id} - ${name}, ${age}`);
 });
-
-app.use('/library-3', (req, res, next) => {
-    console.log('Special request to reserch paper from professor and seniors');
-    next();
-});
-
-app.get('/library-2', (req, res) => {
-    res.send('<h1>Library 2 Entered</h1>');
-})
-
-app.get('/library-3', (req, res) => {
-    res.send('<h1>Library 3 Entered</h1>');
-})
 
 app.listen(PORT, () => {
-    console.log(`Server is up and running on port ${PORT} Ready to handle requests.`);
+    console.log(`Server is up and running on port ${PORT}.`);
 });
--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 
-app.get('/products', (req, res) => {
-    res.send('Here is the list of all products.');
-});
+app.get('/welcome/:username', (req, res) => {
+    const username = req.params.username;
+    const role = req.query.role;
 
-app.post('/products', (req, res) => {
-    res.send('A new product has been added.');
-});
-
-app.get('/categories', (req, res) => {
-    res.send('Here is the list of all categories.');
-});
-
-app.post('/categories', (req, res) => {
-    res.send('A new category has been created.');
-});
-
-app.use((req, res) => {
-    res.status(404).send('<h1>404 - Page Not Found</h1>');
+    res.send(`Welcome ${username}, your role is ${role}`);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is up and running on port ${PORT}.`);
 });

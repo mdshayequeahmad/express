bd228dfd697b47945fbfd348167c3641d2312d73
mdshayequeahmad <shayeque01@gmail.com>
2026-03-17 16:40:15 +0530

    Middleware for Logging

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
-app.get('/welcome/:username', (req, res) => {
-    const username = req.params.username;
-    const role = req.query.role;
-
-    res.send(`Welcome ${username}, your role is ${role}`);
+// Logging Middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} request made to ${req.url}`);
+    next();
 });
 
+// Products
+app.get('/products', (req, res) => {
+    res.send('Here is the list of all products.');
+});
+
+app.post('/products', (req, res) => {
+    res.send('A new product has been added.');
+});
+
+// Categories
+app.get('/categories', (req, res) => {
+    res.send('Here is the list of all categories.');
+});
+
+app.post('/categories', (req, res) => {
+    res.send('A new category has been created.');
+});
+
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}.`);
 });
